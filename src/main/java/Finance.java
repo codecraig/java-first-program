632a9d2508772e3c2ea3084cf7886a4658e55d2a
Craig Watson <48024765+codecraig@users.noreply.github.com>
2022-11-29 15:47:33 +0000

    M5.4-5

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -10,4 +10,18 @@ public class Finance
             BEST_LOAN_RATES, "usage: bestLoanRates",
             SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
             MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>");
+
+    private static boolean validateCommandArguments(String[] args)
+    {
+        switch(args[0])
+        {
+            case BEST_LOAN_RATES:
+                return args.length == 1;
+            case SAVINGS_CALCULATOR:
+                return args.length == 3;
+            case MORTGAGE_CALCULATOR:
+                return args.length == 4;
+        }
+        return false;
+    }
 }
